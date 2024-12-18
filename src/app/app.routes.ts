d69572f295cd7e78f1050d0b24407a225965b7ff
millerdiaz <miller20052022@outlook.com>
2024-12-18 11:55:19 -0500

    se realiza la descripcion de producto dinamica, al igual que la imagen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { CatalogoComponent } from './components/public/catalogo/catalogo.component';
 import { ProductsComponent } from './components/public/products/products.component';
+import { DescripcionComponent } from './component/descripción/descripcion.component';
 
 export const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'catalogo',component:CatalogoComponent},
   {path:'products',component:ProductsComponent},
+  {path:'descripcion',component:DescripcionComponent},
   {path:'',pathMatch:'full', redirectTo:'home'}
 ];
--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component, HostListener } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
