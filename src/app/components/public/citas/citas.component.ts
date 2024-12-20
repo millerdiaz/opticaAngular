1abc1af428a780a965a336ad377bec11c31b8ab3
ana <anaranja111@gmail>
2024-12-19 22:11:03 -0500

    formulario y card

--- src/app/components/public/citas/citas.component.ts
+++ src/app/components/public/citas/citas.component.ts
@@ -1,31 +1,79 @@
-import { Component, NgModule } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 import { CitasService } from '../../../services/citas.service';
+import { CommonModule } from '@angular/common';
+
+
+
 
 @Component({
   selector: 'app-citas',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './citas.component.html',
-  styleUrl: './citas.component.css'
+  styleUrls: ['./citas.component.css'],
+
 })
 export class CitasComponent {
-
-  selectedDepartamento: string = '';
+  citaAgendada: boolean = false
+  cita!: any
+  citasService = inject(CitasService)
   selectedCiudad: string = '';
   selectedTienda: string = '';
   selectedTipoCita: string = '';
   selectedFecha: string = '';
   selectedEspecialista: String='';
-  constructor (private citas: CitasService ){
-  }
+  ////////***** Para seleccionar Cc dependiento de la ciudad */
+  bogota: any = ['Chapinero', 'CC Titan Plaza', 'CC el retiro', 'CC Gran Estación'];
+  medellin: any =  ['CC Oviedo', 'CC El Tesoro', 'CC Viva Envigado']; // CC El Tesoro
+  cali: any = ['Palmetto Plaza', 'Unicentro', 'CC Jardín Plaza'];
+
+  ////////***** Para seleccionar Especialista dependiendo de la ciudad */
+  bogotaEspecialista: any = ['Alberto Martínez', 'Laura Gómez', 'Juan Pérez', 'Sofía Rodríguez'];
+  medellinEspecialista: any =  ['Carlos López', 'Ana Morales', 'Pedro Ramírez'];
+  caliEspecialista: any = ['María Fernández', 'José Torres', 'Lucía Castro'];
+
+
+  // Ciudades y sus respectivas tiendas
+  // ciudadesYTiendas: any = {
+  //   Bogotá: ['Chapinero', 'CC Titan Plaza', 'CC el retiro', 'CC Gran Estación'],
+  //   Medellín: ['CC Oviedo', 'CC El Tesoro', 'CC Viva Envigado'],
+  //   Cali: ['Palmetto Plaza', 'Unicentro', 'CC Jardín Plaza']
+  // };
+
+
+  // ciudades: string[] = Object.keys(this.ciudadesYTiendas);
+  // Lista dinámica de tiendas según la ciudad seleccionada
+  tiendas: string[] = [];
+
+  constructor (private citas: CitasService ){}
+
+//El ngOnInit va después del constructor
+
+ngOnInit(){
+  const citasAgendadas = '6764a7884b22fd492cc61e23'
+  this.citasService.getCita(citasAgendadas).subscribe({
+    next: (resApi: any )=>{
+      this.cita = Array.isArray(resApi)? resApi: [resApi]
+
+    }, error :( error: any ) =>{
+      console.log(error);
+
+    }
+  })
+}
+
+
+
   // Métodos para actualizar las variables cuando se selecciona un valor
-  selectDepartamento(departamento: string) {
-    this.selectedDepartamento = departamento;
-  }
 
   selectCiudad(ciudad: string) {
+    console.log(ciudad);
+
     this.selectedCiudad = ciudad;
+    // this.selectedCiudad = ciudad;
+    // this.tiendas = this.ciudadesYTiendas[ciudad] || []; // Actualiza las tiendas dinámicamente
+    this.selectedTienda = ''; // Resetea la tienda seleccionada
   }
 
   selectTienda(tienda: string) {
@@ -43,7 +91,7 @@ export class CitasComponent {
   // Método para recolectar toda la información y mostrarla en consola
   agendarCita() {
     const datosCita = {
-      departamento: this.selectedDepartamento,
+
       ciudad: this.selectedCiudad,
       tienda: this.selectedTienda,
       tipoDeCita: this.selectedTipoCita,
@@ -51,11 +99,12 @@ export class CitasComponent {
       fecha: this.selectedFecha
     };
      this.addCita(datosCita);
+     this.citaAgendada = true
 
     console.log("Cita Agendada:", datosCita);
 
     // Aquí puedes enviar los datos a un servicio o backend
-    alert('Cita agendada con éxito.');
+
   }
 
 
--- src/app/services/citas.service.ts
+++ src/app/services/citas.service.ts
@@ -6,8 +6,13 @@ import { Injectable } from '@angular/core';
 })
 export class CitasService {
  apiUrl: string = "http://localhost:3000/api/crear"
+ apiUrl2: string = "http://localhost:3000/api"
   constructor(private http:HttpClient ) { }
   createCita(cita: any){
     return this.http.post(this.apiUrl, cita)
   }
+  getCita (id: string){
+    return this.http.get(`${ this.apiUrl2}/buscarCita/${id}`)
+  }
 };
+
